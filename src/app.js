5809f66ad577f4228c642f15dce3c57938377b67
sorin <dragonsorin@gmail.com>
2018-05-17 23:17:57 +0300

    On lesson 43 just after default props

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
-import EmailEditor from 'react-email-editor';
+// import EmailEditor from 'react-email-editor';
 import uuid from 'uuid';
 class App extends Component {
   constructor(props) {
@@ -9,10 +9,11 @@ class App extends Component {
       id: uuid(),
       title: 'React Decision',
       subtitle: 'Decide what to do with React!',
-      options: ['Item one', 'Item two', 'Item four']
+      options: props.options
     }
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
   }
 
   handleDeleteOptions() {
@@ -27,6 +28,22 @@ class App extends Component {
     const random = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[random];
     console.log(option);
+  }
+
+  handleAddOption(option) {
+    // if there is an empty string
+    if(!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      // if we found a mach, our option already exists
+      return 'This option already exists';
+    } 
+
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat(option)
+      }
+    })
   }
 
   render() {
@@ -44,72 +61,89 @@ class App extends Component {
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
         />
-        <AddOption />
+        <AddOption
+          handleAddOption={this.handleAddOption} 
+        />
       </div>
     );
   }
 }
 
-class Header extends Component {
-  render() {
-    return(
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+App.defaultProps = {
+  options: []
 }
 
-class Action extends Component {
- 
-  render() {
-    return(
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: 'some default'
+}
+
+const Action = (props) => {
+  return(
       <div>
         <button 
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
+          onClick={props.handlePick}
+          disabled={!props.hasOptions}
         >Decide what to do with React!
         </button>
       </div>
     );
-  }
+};
+
+
+
+const Options = (props) => {
+  return(
+    <div>
+      <button 
+        onClick={props.handleDeleteOptions}
+      >Remove All
+      </button>
+      <p>Options page</p>
+      {props.options.map((option) => <Option key={option} option={option} />)}
+    </div>
+  );
 }
 
-class Options extends Component {
-  render() {
-    return(
-      <div>
-        <button 
-          onClick={this.props.handleDeleteOptions}
-        >Remove All
-        </button>
-        <p>Options page</p>
-        {this.props.options.map((option) => <Option key={option} option={option} />)}
-      </div>
-    );
-  }
-}
-
-class Option extends Component {
-  render() {
-    return(
-      <div>
-        <p>{this.props.option}</p>
-      </div>
-    );
-  }
+const Option = (props) => {
+  return(
+    <div>
+      <p>{props.option}</p>
+    </div>
+  );
 }
 
 class AddOption extends Component {
+  constructor(props) {
+    super();
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    }
+  }
   handleAddOption(event) {
     event.preventDefault();
     const option = event.target.elements.option.value.trim();
-    console.log(option);
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => {
+      return {
+        error: error
+      }
+    })
   }
   render() {
     return(
       <div>
+        <p>{this.state.error}</p>
         <form onSubmit={this.handleAddOption}>
           <input type="text" name="option" />
           <button>Add Option</button>
@@ -118,7 +152,6 @@ class AddOption extends Component {
     );
   }
 }
-
 
 
 export default App;
